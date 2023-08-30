ac8e5ddfee0d31ac7e48007f7521b4a29fcc723b
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-08-30 14:12:49 -0500

    Replace intro paragraph with a heading

--- components/intro.tsx
+++ components/intro.tsx
@@ -34,7 +34,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -42,7 +42,7 @@ export default function Intro() {
         Hi, I&apos;m Jacob. I&apos;m a software engineer and web
         developer. I specialize in front-end development, but I have
         experience with back-end development as well.
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
