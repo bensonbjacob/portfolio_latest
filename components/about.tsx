d6edf0944ea34a604b43437243035e410e04742d
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-08-30 14:32:17 -0500

    Add About component

---
+++ components/about.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+      <h2 className='text-3xl font-medium capitalize mb-8'>
+        About Me
+      </h2>
+      <p className='mb-3 '>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Veniam ab provident numquam nihil officiis quis labore.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Praesentium ipsa cupiditate perferendis.
+      </p>
+    </section>
+  );
+}
