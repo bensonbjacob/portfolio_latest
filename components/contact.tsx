74f4eef0397ca3397a9a800f7e94225cd0c103c5
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-02 08:47:09 -0500

    feat(contact): Update email and message inputs
    - Added `required` attribute to the email and textarea inputs to ensure it is not left blank.
    - Set `maxLength` attribute to 500 for both email and message inputs to limit the input length.

--- components/contact.tsx
+++ components/contact.tsx
@@ -35,10 +35,16 @@ export default function Contact() {
       <form className='mt-7 flex flex-col'>
         <input
           type='email'
+          required
+          maxLength={500}
           placeholder='Your email'
           className='h-14 px-4 rounded-lg borderBlack'
         />
-        <textarea className='h-52 my-3 rounded-lg borderBlack p-4' />
+        <textarea
+          required
+          maxLength={500}
+          className='h-52 my-3 rounded-lg borderBlack p-4'
+        />
         <button
           className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950'
           type='submit'
