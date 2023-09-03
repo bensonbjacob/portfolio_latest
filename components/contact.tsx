b883548ef88fa79ff1344fac2f89aa8b8064ca74
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-03 17:51:57 -0500

    feat: Add toast notifications for email sending status
    - Added the `react-hot-toast` library to display toast notifications for the email sending process.
    - Imported the `Toaster` component from `react-hot-toast` in the `layout.tsx` file to set the position of the notifications to the top center.
    - Imported the `toast` function from `react-hot-toast` in the `contact.tsx` file to display success or error notifications for email sending status.
    - Used the `toast` function to display success or error notifications after sending an email in the form submission.
    - Updated `package.json` and `package-lock.json` to include the `react-hot-toast` library as a dependency.

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,6 +6,7 @@ import { useSectionInView } from '@/lib/hooks';
 import { motion } from 'framer-motion';
 import { sendEmail } from '@/actions/sendEmail';
 import SubmitBtn from './submit-btn';
+import toast from 'react-hot-toast';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact');
@@ -38,10 +39,10 @@ export default function Contact() {
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
           if (error) {
-            alert(error);
+            toast.error(error);
             return;
           }
-          alert('Email Sent!');
+          toast.success('Email Sent!');
         }}
       >
         <input
