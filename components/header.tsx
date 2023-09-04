a868dc4680e82b69cd909befd25a8399f0cecc10
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-04 15:37:11 -0500

    style: Update header styles and colors for dark mode
    - Updated the background and border colors for the header component.
    - Adjusted the text and hover colors for the navigation links in the header.
    - Changed the background color for the active section indicator.

--- components/header.tsx
+++ components/header.tsx
@@ -14,13 +14,13 @@ export default function Header() {
   return (
     <header className='z-[999] relative'>
       <motion.div
-        className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.07] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75'
+        className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.07] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-mountain-meadow-600 dark:border-mountain-meadow-700 dark:bg-opacity-65'
         initial={{ y: -100, x: '-50%', opacity: 0 }}
         animate={{ y: 0, x: '-50%', opacity: 1 }}
       ></motion.div>
 
       <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium sm:w-[initial] sm:flex-nowrap sm:gap-5'>
           {links.map((link) => (
             <motion.li
               className='h-3/4 flex items-center justify-center relative'
@@ -30,9 +30,9 @@ export default function Header() {
             >
               <Link
                 className={clsx(
-                  'flex w-full items-center justify-center p-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
+                  'flex w-full items-center justify-center p-3 hover:text-gray-950 transition dark:text-gray-100 dark:hover:text-opacity-75',
                   {
-                    'text-gray-950 dark:text-gray-200':
+                    'text-gray-950 dark:text-gray-100 dark:text-opacity-80':
                       activeSection === link.name,
                   }
                 )}
@@ -51,7 +51,7 @@ export default function Header() {
                       stiffness: 400,
                       damping: 30,
                     }}
-                    className='bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800'
+                    className='bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-mountain-meadow-700'
                   ></motion.span>
                 )}
               </Link>
