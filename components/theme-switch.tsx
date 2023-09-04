857b7761fe99a38e4d24633a6be5ac91aa73651d
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-04 08:15:14 -0500

    Add moon icon to dark mode toggle button with
    The code changes update the dark mode toggle button to the theme switch component. The button now displays a moon icon when the theme is set to dark mode.

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { BsSun } from 'react-icons/bs';
+import { BsSun, BsMoon } from 'react-icons/bs';
 
 type Theme = 'light' | 'dark';
 
@@ -21,7 +21,7 @@ export default function ThemeSwitch() {
       className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active-scale-105 transition-all'
       onClick={toggleTheme}
     >
-      <BsSun />
+      {theme === 'light' ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
