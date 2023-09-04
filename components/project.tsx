911602196f9c451d59fe1185e0e54974286c92e0
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-04 17:46:28 -0500

    style: Update project component styles for better readability and consistency.
    - Update background and hover colors for improved visual contrast.
    - Adjust text color for better visibility in dark mode.

--- components/project.tsx
+++ components/project.tsx
@@ -30,10 +30,10 @@ export default function Project({
       ref={ref}
       style={{ scale: scaleProgess, opacity: scrollYProgress }}
     >
-      <article className='bg-gray-100 hover:bg-gray-200 transition max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20 dark:text-white'>
+      <article className='bg-gray-100 hover:bg-gray-200 transition max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 dark:bg-mountain-meadow-300/30 dark:hover:bg-mountain-meadow-300/10 dark:text-white'>
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full flex flex-col sm:group-even:ml-[18rem]'>
           <h3 className='text-2xl font-semibold'>{title}</h3>
-          <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>
+          <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/90'>
             {description}
           </p>
           <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
