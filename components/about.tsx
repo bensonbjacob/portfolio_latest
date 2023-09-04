2f50401d735d99260bbfebe753c7d308b8a2f66f
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-04 17:33:56 -0500

    style: Make About section font bold in dark mode

--- components/about.tsx
+++ components/about.tsx
@@ -10,14 +10,14 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
-      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
+      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 dark:font-semibold'
       id='about'
       initial={{ y: 100, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ delay: 0.25 }}
     >
       <Heading>About Me</Heading>
-      <p className='mb-3 '>
+      <p className='mb-3'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Veniam ab provident numquam nihil officiis quis labore.
       </p>
