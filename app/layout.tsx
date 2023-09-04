58c46be65bca0e6c4842e3233a0dda699e7a92a8
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-04 17:22:09 -0500

    style: Update layout background color and text color in RootLayout component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang='en' className='!scroll-smooth'>
       <body
-        className={`${inter.className} bg-gray-50 dark:bg-gradient-to-r from-mountain-meadow-800 to-mountain-meadow-900  text-gray-950 dark:text-gray-50 relative pt-28 sm:pt-36`}
+        className={`${inter.className} bg-gray-50 dark:bg-gradient-to-r from-mountain-meadow-800 to-mountain-meadow-900  text-gray-950 dark:text-white relative pt-28 sm:pt-36`}
       >
         <div className='bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-mountain-meadow-600'></div>
         <div className='bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[5rem] dark:bg-mountain-meadow-600'></div>
