4142d18308c91f1e0bed277a00f0bfb99fbcef31
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-04 08:51:56 -0500

    Update layout.tsx and theme-switch.tsx
    - Improve layout styling by adding dark mode support
    - Update theme-switch functionality to toggle dark mode and add/remove 'dark' class from document element
    - Update tailwind.config.js to include dark mode support with class-based style selection

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang='en' className='!scroll-smooth'>
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
+        className={`${inter.className} bg-gray-50 dark:bg-gray-900 text-gray-950 dark:text-gray-50 dark:text-opacity-90 relative pt-28 sm:pt-36`}
       >
         <div className='bg-[#FBE2E3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#DBD7FB] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[5rem]'></div>
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -12,9 +12,11 @@ export default function ThemeSwitch() {
     if (theme === 'light') {
       setTheme('dark');
       window.localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
     } else {
       setTheme('light');
       window.localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
     }
   };
 
@@ -24,10 +26,14 @@ export default function ThemeSwitch() {
     ) as Theme | null;
     if (localTheme) {
       setTheme(localTheme);
+      if (localTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+      }
     } else if (
       window.matchMedia('(prefers-color-scheme: dark)').matches
     ) {
       setTheme('dark');
+      document.documentElement.classList.add('dark');
     }
   }, []);
 
