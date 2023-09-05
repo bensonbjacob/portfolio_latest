7b95dd3fc4fa9eacfd7e3e9384513035cafa5b03
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-05 13:15:54 -0500

    Update project images and descriptions
    - Changed project images and descriptions for better visual representation and clarity.
    - Updated the project images to reflect the correct assets.
    - Modified the project descriptions to provide accurate and concise explanations of each project.
    - Updated project links and information for the revised projects.

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,12 @@ import React from 'react';
 import { CgWorkAlt } from 'react-icons/cg';
 import { FaReact } from 'react-icons/fa';
 import { LuGraduationCap } from 'react-icons/lu';
-import corpcommentImg from '@/public/corpcomment.png';
-import rmtdevImg from '@/public/rmtdev.png';
-import wordanalyticsImg from '@/public/wordanalytics.png';
+import promptsImg from '@/public/assets/prompts.png';
+import gutterImg from '@/public/assets/gutter.png';
+import booleImg from '@/public/assets/boole.png';
+import leeCountyImg from '@/public/assets/leecounty.png';
+import propertyImg from '@/public/assets/property.png';
+import restaurantImg from '@/public/assets/restaurant.png';
 
 export const links = [
   {
@@ -62,25 +65,58 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: 'CorpComment',
+    title: 'AI Prompts',
+    github: 'https://github.com/bensonbjacob/ai_prompts',
+    demo: 'https://share-prompts-gamma.vercel.app/',
     description:
-      'I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.',
+      'Prompts allows users to create, discover, and share AI prompts, fostering a collaborative environment for creative writing and ideation. With Prompts, you can unleash your imagination and explore a diverse range of prompts contributed by others.',
     tags: ['React', 'Next.js', 'MongoDB', 'Tailwind', 'Prisma'],
-    imageUrl: corpcommentImg,
+    imageUrl: promptsImg,
   },
   {
-    title: 'rmtDev',
+    title: 'Gutter Calculator',
+    github: 'https://github.com/bensonbjacob/gutter-nextjs',
+    demo: 'https://gutter-nextjs.vercel.app/',
     description:
-      'Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.',
+      'Years ago, I helped my friend with his gutter installation business for a few weeks while he was expanding. I was surprised at how much time he spent composing simple estimates based on the measurements he took on each potential job. I had the idea to create a basic calculator and I filed it away under my list of future projects. When I finally found myself with time to work on projects of my own, the gutter calculator was one of the first I completed.',
     tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: rmtdevImg,
+    imageUrl: gutterImg,
   },
   {
-    title: 'Word Analytics',
+    title: 'Restaurant Website',
+    github: 'https://github.com/bensonbjacob/restaurant-page',
+    demo: 'https://restaurant-page-rosy.vercel.app/',
     description:
-      'A public web app for quick analytics on text. It shows word count, character count and social media post limits.',
+      'This is a React app that serves as the web page for an imaginary restaurant. The site uses custom CSS styles without relying on any frameworks.',
     tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: wordanalyticsImg,
+    imageUrl: restaurantImg,
+  },
+  {
+    title: 'Boole Bots',
+    github: 'https://github.com/chingu-voyages/v44-tier2-team-21',
+    demo: 'https://boolebots21.netlify.app/',
+    description:
+      'Boolebots is an automated boolean logic game that allows users to test their skills in logic and problem solving. The game is built with React and Node.js, and it features a user-friendly interface and challenging levels.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    imageUrl: booleImg,
+  },
+  {
+    title: 'Lee County Deck and Fence',
+    github: 'https://github.com/bensonbjacob/lee-county-deck',
+    demo: 'https://lee-county-deck.vercel.app/',
+    description:
+      'A marketing/landing page for an imaginary deck and fence company.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    imageUrl: leeCountyImg,
+  },
+  {
+    title: 'Property Finder',
+    github: 'https://github.com/bensonbjacob/lcliving',
+    demo: 'https://lcliving.vercel.app/',
+    description:
+      'This is a NextJS app that allows you to filter available properties in the UAE using the free Bayut API. This was my first NextJS project. It allowed me to become familiar with the framework while also allowing me to practice using a new API.',
+    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
+    imageUrl: propertyImg,
   },
 ] as const;
 
