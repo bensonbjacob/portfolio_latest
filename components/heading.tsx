44ad62522880136fc31085320db61d6a65c479fc
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-06 09:35:30 -0500

    Update dark modeHeading component style to include a dark text color

--- components/heading.tsx
+++ components/heading.tsx
@@ -4,7 +4,7 @@ type HeadingProps = {
 
 export default function Heading({ children }: HeadingProps) {
   return (
-    <h2 className='text-3xl font-medium capitalize mb-8 text-center'>
+    <h2 className='text-3xl font-medium capitalize mb-8 text-center dark:text-[rgb(97,244,241)]'>
       {children}
     </h2>
   );
