edea185a0f69605ad87c9c5554f4dacbc3bbb3ca
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-06 11:03:25 -0500

    Update footer styling to use a specific dark text color

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className='mb-10 px-4 text-center text-gray-500 dark:text-gray-300'>
+    <footer className='mb-10 px-4 text-center text-gray-500 dark:text-[rgb(130,175,174)]'>
       <small className='block mb-2 text-xs'>
         &copy; 2023 Jacob Benson
       </small>
