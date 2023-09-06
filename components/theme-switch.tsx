f81b673d3d13a526c76f7d76cb851d7ae6a309e0
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-06 11:08:20 -0500

    Update theme switch button styling for light and dark themes
    The commit updates the `ThemeSwitch` component in `components/theme-switch.tsx` to modify the button's CSS classes for light and dark themes. The changes include adjusting the background color, border color, and other visual properties to align with the desired styling.

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -8,7 +8,7 @@ export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
   return (
     <button
-      className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active-scale-105 transition-all dark:bg-mountain-meadow-900 dark:border-opacity-10'
+      className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active-scale-105 transition-all dark:bg-[rgb(51,68,89)] dark:border-[rgb(71,87,105)]'
       onClick={toggleTheme}
     >
       {theme === 'light' ? <BsSun /> : <BsMoon />}
