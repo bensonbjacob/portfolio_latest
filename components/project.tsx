5edc38ac624dd408b71b6808b8539fadb9c73efd
bensonbjacob <85206755+bensonbjacob@users.noreply.github.com>
2023-09-06 09:48:38 -0500

    Update project component dark mode styling
    - Refactored the styling of the project component to improve readability and consistency.
    - Updated the background and border colors to create a better visual contrast.
    - Made the text color in the dark theme more legible.
    - Adjusted the hover and active states of the GitHub and demo links for better user interaction.
    - Updated the styling of the tags to match the overall design.

--- components/project.tsx
+++ components/project.tsx
@@ -31,32 +31,34 @@ export default function Project({
       ref={ref}
       style={{ scale: scaleProgess, opacity: scrollYProgress }}
     >
-      <article className='bg-gray-100 hover:bg-gray-200 transition max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 dark:bg-mountain-meadow-300/30 dark:hover:bg-mountain-meadow-300/10 dark:text-white'>
+      <article className='bg-gray-100 hover:bg-gray-200 transition max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 dark:bg-transparent dark:border-2 dark:border-[rgb(71,87,105)]'>
         <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] h-full flex flex-col sm:group-even:ml-[18rem]'>
           <div className='flex flex-row items-center'>
-            <h3 className='text-2xl font-semibold'>{title}</h3>
+            <h3 className='text-2xl font-semibold dark:text-[rgb(97,244,241)]'>
+              {title}
+            </h3>
             <a
               href='https://github.com/bensonbjacob'
               target='_blank'
-              className='bg-black/[0.7] borderBlack ml-2 p-2 text-white text-[1.5rem] flex items-center rounded-full outline-none focus:scale-[1.25] hover:scale-[1.25] active:scale-105 transition hover:text-gray-400 dark:bg-white/10 cursor-pointer dark:text-white dark:hover:text-white/60'
+              className='bg-black/[0.7] borderBlack ml-2 p-2 text-white text-[1.5rem] flex items-center rounded-full outline-none focus:scale-[1.25] hover:scale-[1.25] active:scale-105 transition hover:text-gray-400 dark:text-white dark:border dark:border-[rgb(71,87,105)] dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)]'
             >
               <AiFillGithub />
             </a>
             <a
               href='https://github.com/bensonbjacob'
               target='_blank'
-              className='bg-black/[0.7] borderBlack ml-2 p-3 font-semibold text-sm text-white text-[1.5rem] flex items-center rounded-full outline-none focus:scale-[1.25] hover:scale-[1.25] active:scale-105 transition hover:text-gray-400 dark:bg-white/10 cursor-pointer dark:text-white dark:hover:text-white/60'
+              className='bg-black/[0.7] borderBlack ml-2 p-3 font-semibold text-sm text-white text-[1.5rem] flex items-center rounded-full outline-none focus:scale-[1.25] hover:scale-[1.25] active:scale-105 transition hover:text-gray-400 dark:text-white dark:border dark:border-[rgb(71,87,105)] dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)]'
             >
               Demo
             </a>
           </div>
-          <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/90'>
+          <p className='mt-2 leading-relaxed text-gray-700 dark:text-[rgb(130,175,174)]'>
             {description}
           </p>
           <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
             {tags.map((tag, index) => (
               <li
-                className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:bg-mountain-meadow-800/90 dark:text-white/90 dark:group-hover:bg-mountain-meadow-400/70'
+                className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:bg-[rgb(66,153,174)]'
                 key={index}
               >
                 {tag}
