1ac4d2ca17ffb79b848732a34d7c91cd42239085
Jacob <halter_curds.0r@icloud.com>
2023-12-08 12:45:30 -0600

    Refactor intro component layout and improve accessibility
    This commit refactors the layout of the intro component in intro.tsx to enhance visual presentation and improve accessibility. The className attribute of the section element has been corrected, and unnecessary spaces have been removed. The motion animations for the h1 and div elements have been modified to improve visual transitions. The Link, a, and Image components have been adjusted with appropriate classNames and attributes for better accessibility. In addition, the href attribute of the a elements has been updated. These changes enhance the user experience and conform to accessibility guidelines.

--- components/intro.tsx
+++ components/intro.tsx
@@ -23,15 +23,13 @@ export default function Intro() {
     <section
       ref={ref}
       id='home'
-      className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-36 dark:text-[rgb(97,244,241)]'
-    >
+      className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-36 dark:text-[rgb(97,244,241)]'>
       <div className='flex items-center justify-center'>
         <div>
           <motion.div
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
-            transition={{ type: 'tween', duration: 0.3 }}
-          >
+            transition={{ type: 'tween', duration: 0.3 }}>
             <Image
               src={JacobImg}
               alt='Jacob Benson image'
@@ -46,49 +44,43 @@ export default function Intro() {
       <motion.h1
         className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        Hi, I&#39;m Jacob. I&#39;m a full-stack web developer based in
-        Auburn, AL.
+        animate={{ opacity: 1, y: 0 }}>
+        Hi, I&#39;m Jacob. I&#39;m a web developer based in Auburn,
+        AL.
       </motion.h1>
 
       <motion.div
         className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.2 }}
-      >
+        transition={{ delay: 0.2 }}>
         <Link
           href='/#contact'
           onClick={() => {
             setActiveSection('Contact');
             setTimeOfLastClick(Date.now());
           }}
-          className='bg-gray-900 group text-white px-7 py-3 flex items-center rounded-full gap-2 outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition dark:bg-transparent dark:hover:bg-[rgb(63,85,104)] dark:border dark:border-[rgb(61,83,101)] dark:hover:border-[rgb(130,175,174)]'
-        >
+          className='bg-gray-900 group text-white px-7 py-3 flex items-center rounded-full gap-2 outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition dark:bg-transparent dark:hover:bg-[rgb(63,85,104)] dark:border dark:border-[rgb(61,83,101)] dark:hover:border-[rgb(130,175,174)]'>
           Contact Me{' '}
           <AiTwotoneMail className='opacity-90 group-hover:translate-x-2 transition' />
         </Link>
         <a
           href='/public/assets/Jacob_Benson_Resume.pdf'
           download
-          className='bg-white group borderBlack px-7 py-3 flex items-center rounded-full gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)] cursor-pointer dark:text-white dark:border dark:border-[rgb(71,87,105)]'
-        >
+          className='bg-white group borderBlack px-7 py-3 flex items-center rounded-full gap-2 outline-none focus:scale-110 hover:scale-110 active:scale-105 transition dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)] cursor-pointer dark:text-white dark:border dark:border-[rgb(71,87,105)]'>
           Download Resume{' '}
           <BsCloudDownloadFill className='opacity-90 group-hover:translate-y-1 transition' />
         </a>
         <a
           href='https://www.linkedin.com/in/jacob-benson-885004240/'
           target='_blank'
-          className='bg-white borderBlack p-4 text-gray-700 text-[1.5rem] flex items-center rounded-full gap-2 outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition hover:text-gray-950 cursor-pointer dark:text-white dark:border dark:border-[rgb(71,87,105)] dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)]'
-        >
+          className='bg-white borderBlack p-4 text-gray-700 text-[1.5rem] flex items-center rounded-full gap-2 outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition hover:text-gray-950 cursor-pointer dark:text-white dark:border dark:border-[rgb(71,87,105)] dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)]'>
           <AiFillLinkedin />
         </a>
         <a
           href='https://github.com/bensonbjacob'
           target='_blank'
-          className='bg-white borderBlack p-4 text-gray-700 text-[1.5rem] flex items-center rounded-full gap-2 outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition hover:text-gray-950 cursor-pointer dark:text-white dark:border dark:border-[rgb(71,87,105)] dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)]'
-        >
+          className='bg-white borderBlack p-4 text-gray-700 text-[1.5rem] flex items-center rounded-full gap-2 outline-none focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition hover:text-gray-950 cursor-pointer dark:text-white dark:border dark:border-[rgb(71,87,105)] dark:bg-[rgb(51,68,89)] dark:hover:bg-[rgb(63,85,104)]'>
           <AiFillGithub />
         </a>
       </motion.div>
