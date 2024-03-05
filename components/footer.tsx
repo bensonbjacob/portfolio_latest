031c2a5de09152d1608e44409253e71cfcd95b78
Jacob <jacob.benson.dev@gmail.com>
2024-03-05 17:29:13 -0600

    Refactor footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer className='mb-10 px-4 text-center text-gray-500 dark:text-[rgb(130,175,174)]'>
-      <small className='block mb-2 text-xs'>
+    <footer className="mb-10 px-4 text-center text-gray-500 dark:text-[rgb(130,175,174)]">
+      <small className="block mb-2 text-xs">
         &copy; 2023 Jacob Benson
       </small>
-      <p className='text-xs'>
-        <span className='font-semibold'>About this website:</span>{' '}
-        built with React & Next.js, TypeScript, Tailwind CSS, Framer
-        Motion, React Email, Resend, Vercel hosting.
-      </p>
     </footer>
   );
 }
